64896b45077d39c3439607ccfab7a3a7f555510b
D1me9rol <joudindmitriy1@gmail.com>
2024-12-25 13:25:42 +0300

    Добавьте файлы проекта.

---
+++ Models/Book.cs
@@ -0,0 +1,14 @@
+﻿namespace Yudin_back.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public List<string> Author { get; set; }
+        public int Year { get; set; }
+        public bool IsAvailable { get; set; }
+
+
+
+    }
+}
